37814ed5f13be4d019a66adcab233acf474911fb
dan-jc-h <114104608+dan-jc-h@users.noreply.github.com>
2022-12-15 14:18:04 -0500

    Day 4 part 1 done

---
+++ day04/README.md
@@ -0,0 +1,9 @@
+# day04
+
+<https://adventofcode.com/2022/day/4>
+
+## Day 4 Part 1
+
+See details of problem on website.  This is a set problem.  Take each range pair, find intersection, if intersection equals one of the original ranges (sets), then one fully contains the other.
+
+## Day 4 Part 2
---
+++ day04/day04-part1.py
@@ -0,0 +1,30 @@
+# 2022 Advent of Code - Day 4 part 1
+#
+# Read pairs of ranges from file, we need to find if one range is entirely contained in the other.
+# Using a set approach, find intersection, if intersection equals one of the original ranges (sets), 
+# then one fully contains the other.
+
+inputFileName = "day04-input-data.txt"
+#inputFileName = "day04-sample-data.txt"
+
+rangesEnveloped = 0
+with open(inputFileName, 'r') as inputFile:
+    for line in inputFile:
+        line=line.strip()
+        #break line into sections
+        section1,section2=line.split(',')
+        #get ends of each section
+        low1,high1=section1.split('-')
+        low2,high2=section2.split('-')
+        #build ranges (need the +1, because python)
+        range1=range(int(low1),int(high1)+1)
+        range2=range(int(low2),int(high2)+1)
+        #find intersection
+        common = set(range1).intersection(range2)
+        #print(line , range1, range2, common)
+        # if either range equals the intersection of the two ranges, one range is completely enveloped
+        if set(range1)==common or set(range2)==common:
+            rangesEnveloped = rangesEnveloped + 1
+
+print(f'Part1: {rangesEnveloped} ranges where one completely envelopes the other.')
+        
