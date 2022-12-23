5aa4a0e7be92fa54b724971742b76126bd979088
dan-jc-h <114104608+dan-jc-h@users.noreply.github.com>
2022-12-23 07:24:41 -0500

    Day 10 - tidied up enough

--- day10/README.md
+++ day10/README.md
@@ -9,6 +9,11 @@ We need to step through a simple program and calculate some metrics around the r
 we are into the program.  The "challenge" seems to be that one instruction `noop` takes a single cycle to execute, while the other 
 instruction `addx` (add to the register) takes two.
 
-Presumably Part 2 will introduce additional instructions, so I'll try to generalize how the instructions are handled.
+Presumably Part 2 will introduce additional instructions (spoiler: it doesn't), so I'll try to generalize how the instructions are handled.
 
 I always wanted to write a simple CPU simulator... maybe this will give me some ideas/inspiration!
+
+## Day 10 Part 2
+
+Add a display with some really funky rules about how it's updated.  This took some tweaking to match the 
+reauired conditions, but I got it working.
--- day10/day10.py
+++ day10/day10.py
@@ -6,17 +6,31 @@
 from enum import Enum
 
 class CPUState(Enum):
+    """
+    Enum Representing the state the CPU is in.  Only two options in our simple case.
+    """
     PROCESSING = 1
     WAITING =2
 
 class Instruction:
+    """
+    Class representing a CPU instruction.  Important attributes are the opcode, execution time and a function that actually performs the instruction
+    """
     def __init__(self,_opcode:str,_executionTime:int,_fn) -> None:
         self.opcode = _opcode
         self.executionTime = _executionTime
         self.fn = _fn
 
 class Cpu:
+    """
+    class representing a CPU. Important attributes are a set of valid instructions, the X register, the CPU state, the instruction currently being executed and it's parameters, number of cycles remaining in the active instruction.
+
+    Normal use is to check the status, if WAITING, load an instruction and cycle the machine until it's WAITING again.
+    """
     def __init__(self) -> None:
+        """
+        CPU initialization, valid instructions need to be set.
+        """
         self.instructions=set() # dict?
         self.X=1
         self.state=CPUState.WAITING
@@ -24,45 +38,63 @@ class Cpu:
         self.currentInstructionParms=""
         self.cyclesRemaining=0
     def execute(self)-> None:
-        #find the fn
-        for inst in self.instructions:
-            if inst.opcode == self.currentInstruction:
-                inst.fn(self,self.currentInstructionParms)
+        """
+        execute the currently loaded instruction by calling its function
+        """
+        self.getInstruction(self.currentInstruction).fn(self,self.currentInstructionParms)
     def __str__(self) -> str:
         return f'state: {self.state.name} X={self.X}'
     def getInstruction(self,opcode:str)->Instruction:
+        """
+        get an instruction, given an opcode
+
+        Args:
+            opcode (str): the text opcode for an instruction
+
+        Returns:
+            Instruction: the requested instruction, None if not found
+        """
         for i in self.instructions:
             if i.opcode==opcode:
                 return i
-        return None #FIXME - this should be an exception
+        return None #TODO - this should maybe be an exception
     def loadInstruction(self,opcode:str,parms:str)->None:
-        #print(f"Loading Instruction: {opcode}({parms}) ")
+        """
+        load an instruction into the CPU to prepare for execution, set state to PROCESSING.  execution actually occurs at the END of the required clock cycles for this instruction.  E.g. register will not be updated until the last cycle of it's execution.  In previous cycles register will be unchanged.
+
+        Args:
+            opcode (str): The opcode for the instruction
+            parms (str): any params (watch out... str, may need to convert to int to do anything in the instruction function)
+        """
         self.currentInstruction=self.getInstruction(opcode).opcode
         self.currentInstructionParms=parms
         self.cyclesRemaining=self.getInstruction(self.currentInstruction).executionTime-1
         self.state=CPUState.PROCESSING
     def cycle(self) -> None:
-        # if there are cycles remaining in the current instruction:
-        #   - decrement cycles remaining
-        # if cycles remaining hits zero - execute instruction, and set state to WAITING
+        """apply a clock cycle to the CPU. If there are cycles remaining in the current instruction, decrement cycles remaining; if cycles remaining is zero - execute instruction by calling its function, and set state to WAITING
+        """
         if self.cyclesRemaining > 0:
             self.cyclesRemaining = self.cyclesRemaining - 1
         else:
             self.execute()
             self.state=CPUState.WAITING
 
+# Dimensions for our CRT display
 CRT_WIDTH = 40
 CRT_HEIGHT = 6
 
 class CRT:
+    """represents a CRT display, contains the dimensions and a buffer (list of lists... one list per line)
+    """
     def __init__(self):
+        """initialize display elements and set array to dots"""
         self.width=CRT_WIDTH
         self.height=CRT_HEIGHT
         self.buffer=[]
         for h in range(self.height):
             self.buffer.append([])
             for w in range(self.width):
-                self.buffer[h].append(" ")
+                self.buffer[h].append(".")
     def __str__(self):
         output=""
         for h in range(self.height):
@@ -71,16 +103,40 @@ class CRT:
             output=output+"\n"
         return (output)
     def update(self,_cpu:Cpu,x:int,y:int):
+        """update the display.  The (annoying) rules are, that a particular element should be updated only if the x location in the row is equal to the current X register value... +/- 1.
+
+        Args:
+            _cpu (Cpu): the CPU
+            x (int): X location
+            y (int): Y location
+        """
         x=x-1 # annoying correction
         if x>=_cpu.X-1 and x<=_cpu.X+1 :
             print(f'  in display.update -> x:{x} y:{y} X-reg={_cpu.X}')
             self.buffer[y][x]="#"
 
-def fnNoop(c:Cpu,p:str):
+#
+# Definitions of CPU Instruction functions.  REQUIRED PARMs are:
+#   The CPU, and
+#   a string containing parameters (which may be ignored)
+#
+def fnNoop(c:Cpu,p:str)->None:
+    """noop CPU Instruction - do nothing
+
+    Args:
+        c (Cpu): CPU
+        p (str): Optional arguments (ignored)
+    """
     #print(":fnNoop")
     pass
 
-def fnAddx(c:Cpu,y:int):
+def fnAddx(c:Cpu,y:int)->None:
+    """addx CPU instruction - add a value to the X register
+
+    Args:
+        c (Cpu): CPU
+        y (int): value to add (will need to be cast to int)
+    """
     #print(":fnAddx")
     c.X = c.X + int(y)
 
@@ -90,7 +146,7 @@ def fnAddx(c:Cpu,y:int):
 inputFileName = 'day10/day10-input-data.txt'
 
 
-#build the virtual cpu
+#build the virtual cpu and add instructions
 adventOMatic9000 = Cpu()
 adventOMatic9000.instructions.add(Instruction("noop",1,fnNoop))
 adventOMatic9000.instructions.add(Instruction("addx",2,fnAddx))
@@ -114,6 +170,7 @@ with open(inputFileName, 'r') as inputFile:
             programlineNumber = programlineNumber + 1
             if line == '':
                 break
+            #minor trick here to make sure we get at least two elements.  Maybe should have used re
             opcode,operand=(((line.strip())+" # #").split())[0:2]
             if operand == '#':
                 operand = ''
@@ -134,8 +191,5 @@ with open(inputFileName, 'r') as inputFile:
 
 print(f'Program complete after {cycleCounter} cycles and {programlineNumber} lines of instructions.')
 print(f'Signal Strength: {signalStrength}')
-
-
-
 print(display)
 
