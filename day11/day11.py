c3be34da616c72ad872518ab0a173d15632e7658
dan-jc-h <114104608+dan-jc-h@users.noreply.github.com>
2022-12-24 11:41:34 -0500

    day11 -part1 tidied up

--- day11/day11.py
+++ day11/day11.py
@@ -1,10 +1,14 @@
 # 2022 Advent of Code - Day 11
 #   https://adventofcode.com/2022/day/11
 # Part 1 - monkeys throwing stuff around.
+#
+# note: this is would be very sensitive to problems in the input file
 
 import re
 
 class Monkey:
+    """represents a monkey.  Important aspects of the monkey's decision making are stored as attributes
+    """
     def __init__(self,_items,_operation,_testDivisor:int,_trueDest:int,_falseDest:int):
         self.items=_items
         self.operation=_operation
@@ -16,13 +20,18 @@ class Monkey:
         output = f'items: {self.items}, op: {self.operation}, div:{self.testDivisor}, T/F:{self.trueDest}/{self.falseDest} inspects:{self.inspectionCount}'
         return output
 
+#show me the monkeys!
+def showMeTheMonkeys(troop:[])->None:
+    for i,m in enumerate(troop):
+        print(f"Monkey {i}: {m}")
+
 inputFileName = "day11/day11-sample-data.txt"
 inputFileName = "day11/day11-input-data.txt"
 
-#our troop of monkeys:
+#our troop of monkeys...
 troop=[]
 with open(inputFileName, 'r') as inputFile:
-    # get a monkey descriptor
+    # get a monkey descriptor - monkey descriptions are divided by double newlines
     for monkeyChunk in inputFile.read().split("\n\n"):
         print("- "*20)
         print(monkeyChunk)
@@ -42,11 +51,13 @@ with open(inputFileName, 'r') as inputFile:
                 # convert each item to an int
                 for i,item in enumerate(monkeyItems):
                     monkeyItems[i]=int(item)
+                # need to reverse this to get the stack operations to work as per examples, 
+                # I don't think this actually matters in terms of solving the problem.
                 monkeyItems.reverse()
                 print(f'      items: {monkeyItems}')
             elif monkeyProperty.lower() == "operation":
                 #This is always format: new = old <operator> <num or "old">
-                # so just need last two tokens
+                # so just need last two tokens, the operator and either a number or "old"
                 monkeyOperation=(monkeyPropertyText.rsplit(" ",2))[-2:]
                 print(f'      operation: {monkeyOperation}')
                 pass
@@ -55,9 +66,11 @@ with open(inputFileName, 'r') as inputFile:
                 monkeyDivisor=int((monkeyPropertyText.split(' '))[-1])
                 print(f'      divisor: {monkeyDivisor}')
             elif monkeyProperty.lower() == "if true":
+                #number of the monkey that this monkey throws an item to if the remainder operation is true
                 monkeyThrowOnTrue=int((monkeyPropertyText.split(' '))[-1])
                 print(f'      on true, throw to: {monkeyThrowOnTrue}')
             elif monkeyProperty.lower() == "if false":
+                #number of the monkey that this monkey throws an item to if the remainder operation is true
                 monkeyThrowOnFalse=int((monkeyPropertyText.split(' '))[-1])
                 print(f'      on false, throw to: {monkeyThrowOnFalse}')
             else:
@@ -66,27 +79,32 @@ with open(inputFileName, 'r') as inputFile:
             #make a monkey and add to the troop
         troop.append(Monkey(monkeyItems,monkeyOperation,monkeyDivisor,monkeyThrowOnTrue,monkeyThrowOnFalse))
 
-#show me the monkeys!
-for i,m in enumerate(troop):
-    print(f"Monkey {i}: {m}")
+
+showMeTheMonkeys(troop)
 
 MAX_ROUNDS=20
 round=0
 while round<MAX_ROUNDS:
     round=round+1
+    # go through the list of monkeys...
     for mnum,m in enumerate(troop):
+        # go through this monkey's items
         while m.items:
             item=m.items.pop()
             print(f'Monkey {mnum} examines item {item}')
+            # keep track of inspection count for later book keeping
             m.inspectionCount = m.inspectionCount + 1
+            #execute the monkey's operation on the item
             if m.operation[1]=='old':
                 expression = f'item {m.operation[0]} {item}'
             else:
                 expression = f'item {m.operation[0]} {m.operation[1]}'
             item = eval(expression)
             print(f'Monkey {mnum} applies operation to item... {expression} -> {item}')
+            # divide item value by 3.0
             item = int(item/3.0)
             print(f'Monkey gets bored and worry drops by 1/3... {item}')
+            # decide which monkey to trow the item to
             if item % m.testDivisor == 0:
                 print(f'Monkey {mnum} throws item with worry level {item} to Monkey {m.trueDest}')
                 troop[m.trueDest].items.append(item)
@@ -94,17 +112,15 @@ while round<MAX_ROUNDS:
                 print(f'Monkey {mnum} throws item with worry level {item} to Monkey {m.falseDest}')
                 troop[m.falseDest].items.append(item)
     print(f'----------\nEND OF ROUND {round}')
-    for i,m in enumerate(troop):
-        print(f"Monkey {i}: {m}")
+    showMeTheMonkeys(troop)
 
+#finish up by calculations monkey business factor, which only considers the two busiest monkeys.
 mostBusyMonkeys=[]
 for m in troop:
     mostBusyMonkeys.append(m.inspectionCount)
-print(mostBusyMonkeys)
 mostBusyMonkeys.sort()
-print(mostBusyMonkeys)
-mostBusyMonkeys=mostBusyMonkeys[-2:]
-print(mostBusyMonkeys)
+mostBusyMonkeys.reverse()
+# monkey business only considers the two busiest monkeys
 monkeyBusiness = mostBusyMonkeys[0]*mostBusyMonkeys[1]
 print(f'After {MAX_ROUNDS}, Monkey Business = {monkeyBusiness}')
 
