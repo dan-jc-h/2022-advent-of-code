62871b21afc6bbb9f078c8573f2759352173c102
dan-jc-h <114104608+dan-jc-h@users.noreply.github.com>
2023-01-02 14:01:16 -0500

    Added day templates

---
+++ README-template.md
@@ -0,0 +1,8 @@
+# Advent of Code 2022 - Day xx
+
+<https://adventofcode.com/2022/day/xx>
+
+## Day xx Part 1
+
+## Day xx Part 2
+
---
+++ dayxx-template.py
@@ -0,0 +1,24 @@
+# 2022 Advent of Code - Day xx
+#   https://adventofcode.com/2022/day/xx
+#
+# Part 1 -
+#
+# Part 2 -
+
+
+# # # # # # # #  
+#   M A I N   #
+# # # # # # # #
+print(" "+ "* "*19)
+print("* * * ADVENT OF CODE 2022 - DAY xx * * *")
+print("* * *          - Part 1 -          * * *")   
+print(" "+ "* "*19)                
+# where to find input
+inputFileName="dayxx/dayxx-sample-data.txt"
+inputFileName="dayxx/dayxx-input-data.txt"
+print(f"Loading data from: {inputFileName}")
+
+
+print(" "+ "* "*19)
+print("* * *          - Part 2 -          * * *")   
+print(" "+ "* "*19)  
